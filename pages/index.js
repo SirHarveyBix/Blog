69253a52201c24bdd82cdd6d2406c9320da45668
Guillaume <Gui.lefort.17@gmail.com>
2022-02-28 21:10:37 +0100

    tryiong something else

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import FeaturedPosts from '/src/components/homePage/FeaturedPosts/index';
 import Hero from '/src/components/homePage/Hero/index';
 import Head from 'next/head';
-import { ApolloServer } from 'apollo-server';
-import typeDefs from '../utils/schema/typeDefs';
-import resolvers from '../utils/schema/resolvers';
+
 import { FEATURED_POSTS } from '../src/graphql/query';
 import { initializeApollo } from '../utils/apolloClient';
 import { client } from './api/graphql';
@@ -24,16 +22,6 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
-  const server = new ApolloServer({
-    ssrMode: typeof window === 'undefined',
-    resolvers,
-    typeDefs,
-  });
-
-  server.listen().then(({ url }) => {
-    console.log(`🚀  Server ready at ${url}`);
-  });
-
   const apolloClient = initializeApollo();
   const { data } = await client.query({
     query: FEATURED_POSTS,
---
+++ utils/server.js
@@ -0,0 +1,16 @@
+import { ApolloServer } from 'apollo-server';
+import { applyMiddleware } from 'graphql-middleware';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import resolvers from './schema/resolvers';
+import typeDefs from './schema/typeDefs';
+
+const schema = applyMiddleware(makeExecutableSchema({ typeDefs, resolvers }));
+
+const server = new ApolloServer({
+  ssrMode: typeof window === 'undefined',
+  schema,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
