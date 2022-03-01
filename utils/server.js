ad0e20e509b44a0e7fca8101c7a0144f17a7cb27
Guillaume <Gui.lefort.17@gmail.com>
2022-03-01 13:25:13 +0100

    update to push to heroku

--- utils/server.js
+++ utils/server.js
@@ -3,12 +3,15 @@ import { applyMiddleware } from 'graphql-middleware';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import resolvers from './schema/resolvers.js';
 import typeDefs from './schema/typeDefs.js';
+import { ApolloServerPluginInlineTrace } from 'apollo-server-core';
 import 'dotenv/config';
+
 const schema = applyMiddleware(makeExecutableSchema({ typeDefs, resolvers }));
 
 const server = new ApolloServer({
   ssrMode: typeof window === 'undefined',
   schema,
+  plugins: [ApolloServerPluginInlineTrace()],
 });
 console.log(process.env.PORT);
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
