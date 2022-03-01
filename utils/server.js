31f32de4ccc5f16cae28e3ee6e74543a0bab44b8
Guillaume <Gui.lefort.17@gmail.com>
2022-03-01 12:37:56 +0100

    update to push to heroku

--- utils/server.js
+++ utils/server.js
@@ -3,14 +3,14 @@ import { applyMiddleware } from 'graphql-middleware';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import resolvers from './schema/resolvers.js';
 import typeDefs from './schema/typeDefs.js';
-import 'dotenv';
+import 'dotenv/config';
 const schema = applyMiddleware(makeExecutableSchema({ typeDefs, resolvers }));
 
 const server = new ApolloServer({
   ssrMode: typeof window === 'undefined',
   schema,
 });
-
-server.listen().then(({ url }) => {
+console.log(process.env.PORT);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
