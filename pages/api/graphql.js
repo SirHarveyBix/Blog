cad020971e356fd576449c00baba531a549bd7ff
Guillaume <Gui.lefort.17@gmail.com>
2022-03-02 12:57:18 +0100

    edit client : remove shcema for deployement

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,17 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { applyMiddleware } from 'graphql-middleware';
-
-import resolvers from '/utils/schema/resolvers.js';
-import typeDefs from '/utils/schema/typeDefs.js';
 
 export const config = { api: { bodyParser: false } };
 
-const schema = applyMiddleware(makeExecutableSchema({ typeDefs, resolvers }));
-
 export const client = new ApolloClient({
   ssrMode: typeof window === 'undefined',
-  schema,
   uri: `${process.env.URI}`,
   cache: new InMemoryCache(),
 });
