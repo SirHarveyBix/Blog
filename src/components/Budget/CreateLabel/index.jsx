b31ed801433e8d7efbfda0e13b5864396e09b84e
SirHarveyBix <gui.lefort.17@gmail.com>
2022-03-15 16:23:42 +0100

    refacto

---
+++ src/components/Budget/CreateLabel/index.jsx
@@ -0,0 +1,68 @@
+import { useMutation } from '@apollo/client';
+import { useSession } from 'next-auth/react';
+import { useContext, useState } from 'react';
+
+import { CREATE_BUDGET_LINE } from '../../../graphql/query';
+import { NotificationContext } from '../../context/NotificationContext';
+import { Button, Container, Input, Label, ValidButton } from './style';
+
+const CreateLabel = () => {
+  const { data: session, status } = useSession();
+  const { setRequestStatus } = useContext(NotificationContext);
+  const [inputData, setInputData] = useState({ amount: 0, label: '', author: [session?.user] });
+  const [newLabel, setNewLabel] = useState(false);
+
+  const [createBudgetLine] = useMutation(CREATE_BUDGET_LINE, {
+    onCompleted: () => setNewLabel(false),
+  });
+
+  const handleNewLabel = (event) => {
+    event.preventDefault();
+    setNewLabel(true);
+  };
+
+  const handleAddedInput = () => {
+    if (session && status === 'authenticated') {
+      if (!inputData.amount || !inputData.label) {
+        setRequestStatus('missingField');
+        setNewLabel(false);
+        return;
+      }
+      createBudgetLine({ variables: { data: inputData } });
+    }
+  };
+
+  return (
+    <>
+      {newLabel && (
+        <Container>
+          <Label htmlFor="label" />
+          <Input
+            newInput
+            placeholder="Nom du Label"
+            type="text"
+            id="label"
+            onChange={(event) => setInputData({ ...inputData, label: String(event.target.value) })}
+          />
+          <Label htmlFor="amount" />
+          <Input
+            newInput
+            placeholder="Montant"
+            type="number"
+            id="amount"
+            onChange={(event) => setInputData({ ...inputData, amount: Number(event.target.value) })}
+          />
+          <ValidButton
+            width={40}
+            height={40}
+            src="/images/site/valid-Icon.png"
+            onClick={handleAddedInput}
+          />
+        </Container>
+      )}
+      <Button onClick={handleNewLabel}>ajouter un label</Button>
+    </>
+  );
+};
+
+export default CreateLabel;
--- src/components/Budget/index.jsx
+++ src/components/Budget/index.jsx
@@ -1,9 +1,6 @@
-import { useMutation } from '@apollo/client';
-import { useSession } from 'next-auth/react';
-import { useState } from 'react';
-
-import { CREATE_BUDGET_LINE } from '../../graphql/query';
+import { NotificationContextProvider } from '../context/NotificationContext';
 import Notification from '../Notification/index';
+import CreateLabel from './CreateLabel';
 import EditBudget from './EditBudget';
 import {
   Button,
@@ -19,79 +16,28 @@ import {
 
 function Budget(props) {
   const { data, loading } = props;
-  const { data: session, status } = useSession();
-  const [inputData, setInputData] = useState({ amount: 0, label: '', author: [session?.user] });
-  const [newLabel, setNewLabel] = useState(false);
-  const [requestStatus, setSequestStatus] = useState(null);
-
-  const [createBudgetLine] = useMutation(CREATE_BUDGET_LINE, {
-    onCompleted: () => setNewLabel(false),
-    onError: (error) => console.error(error),
-  });
-
-  const handleNewLabel = (event) => {
-    event.preventDefault();
-    setNewLabel(true);
-  };
-
-  const handleAddedInput = () => {
-    if (session && status === 'authenticated') {
-      if (!inputData.amount || !inputData.label) {
-        setSequestStatus('missingField');
-        setNewLabel(false);
-        return;
-      }
-      createBudgetLine({ variables: { data: inputData } });
-    }
-  };
 
   return (
     <>
       <Spacer />
-      <Container>
-        <ContentFrom>
-          <Title>Budget</Title>
-          {loading && !data ? (
-            <p>Loading ..</p>
-          ) : (
-            data?.map((input) => (
-              <div key={input.id}>
-                <EditBudget data={input} />
-              </div>
-            ))
-          )}
-          {newLabel && (
-            <NewLabelControl>
-              <Label htmlFor="label" />
-              <Input
-                newInput
-                placeholder="Nom du Label"
-                type="text"
-                id="label"
-                onChange={(event) => setInputData({ ...inputData, label: event.target.value })}
-              />
-              <Label htmlFor="amount" />
-              <Input
-                newInput
-                placeholder="Montant"
-                type="number"
-                id="amount"
-                onChange={(event) =>
-                  setInputData({ ...inputData, amount: Number(event.target.value) })
-                }
-              />
-              <ValidButton
-                width={40}
-                height={40}
-                src="/images/site/valid-Icon.png"
-                onClick={handleAddedInput}
-              />
-            </NewLabelControl>
-          )}
-          <Button onClick={handleNewLabel}>ajouter un label</Button>
-        </ContentFrom>
-      </Container>
-      <Notification requestStatus={requestStatus} />
+      <NotificationContextProvider>
+        <Container>
+          <ContentFrom>
+            <Title>Budget</Title>
+            {loading && !data ? (
+              <p>Loading ..</p>
+            ) : (
+              data?.map((input) => (
+                <div key={input.id}>
+                  <EditBudget data={input} />
+                </div>
+              ))
+            )}
+            <CreateLabel />
+          </ContentFrom>
+        </Container>
+        <Notification />
+      </NotificationContextProvider>
     </>
   );
 }
