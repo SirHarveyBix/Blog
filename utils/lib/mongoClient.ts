3f99a5641975a98da39c7ec4411d89bcd7d8784e
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2024-06-13 17:16:09 +0200

    fix: db connection

--- utils/lib/mongoClient.ts
+++ utils/lib/mongoClient.ts
@@ -15,13 +15,11 @@ const clientDB = async (db: string): Promise<MongoClient | unknown> => {
     chosenDB =
       PRODUCTION_ENV() === 'production' ? AUTH_DB_PROD() : AUTH_DB_DEV();
   }
-  const connectionString = `mongodb+srv://${USERNAME()}:${PASSWORD()}@${CLUSTER()}.wyrhp.mongodb.net/${chosenDB}?retryWrites=true&w=majority`;
-
-  console.log("XXXXXXX", connectionString)
+  const connectionString = CONNECTION_STRING(chosenDB as string);
 
   let client: MongoClient;
   try {
-    client = await MongoClient.connect(connectionString);
+    client = await MongoClient.connect(String(connectionString));
   } catch (error) {
 
     console.error(error);
@@ -32,48 +30,6 @@ const clientDB = async (db: string): Promise<MongoClient | unknown> => {
 };
 
 export default clientDB;
-
-export const BUDGET_DB_PROD = () => {
-  if (process.env.BUDGET_DB_PROD != null) {
-    return process.env.BUDGET_DB_PROD
-  }
-}
-
-export const BUDGET_DB_DEV = () => {
-  if (process.env.BUDGET_DB_DEV != null) {
-    return process.env.BUDGET_DB_DEV
-  }
-}
-
-export const PRODUCTION_ENV = () => {
-  if (process.env.NODE_ENV != null) {
-    return process.env.NODE_ENV
-  }
-}
-
-export const DB_PROD = () => {
-  if (process.env.DB_PROD != null) {
-    return process.env.DB_PROD
-  }
-}
-
-export const DB_DEV = () => {
-  if (process.env.DB_DEV != null) {
-    return process.env.DB_DEV
-  }
-}
-
-export const AUTH_DB_PROD = () => {
-  if (process.env.AUTH_DB_PROD != null) {
-    return process.env.AUTH_DB_PROD
-  }
-}
-
-export const AUTH_DB_DEV = () => {
-  if (process.env.AUTH_DB_DEV != null) {
-    return process.env.AUTH_DB_DEV
-  }
-}
 
 export const USERNAME = () => {
   if (process.env.USERNAME != null) {
@@ -93,8 +49,51 @@ export const CLUSTER = () => {
   }
 }
 
+const CONNECTION_STRING = (chosenDB: string): string => {
+  return `mongodb+srv://${USERNAME()}:${PASSWORD()}@${CLUSTER()}.nx4tb4v.mongodb.net/${chosenDB}?retryWrites=true&w=majority`;
+}
+
+export const BUDGET_DB_PROD = () => {
+  if (process.env.BUDGET_DB_PROD != null) {
+    return process.env.BUDGET_DB_PROD
+  }
+}
+export const BUDGET_DB_DEV = () => {
+  if (process.env.BUDGET_DB_DEV != null) {
+    return process.env.BUDGET_DB_DEV
+  }
+}
+
+export const DB_PROD = () => {
+  if (process.env.DB_PROD != null) {
+    return process.env.DB_PROD
+  }
+}
+export const DB_DEV = () => {
+  if (process.env.DB_DEV != null) {
+    return process.env.DB_DEV
+  }
+}
+
+export const AUTH_DB_PROD = () => {
+  if (process.env.AUTH_DB_PROD != null) {
+    return process.env.AUTH_DB_PROD
+  }
+}
+export const AUTH_DB_DEV = () => {
+  if (process.env.AUTH_DB_DEV != null) {
+    return process.env.AUTH_DB_DEV
+  }
+}
+
 export const APOLLO_GRAPH_REF = () => {
   if (process.env.APOLLO_GRAPH_REF != null) {
     return process.env.APOLLO_GRAPH_REF
+  }
+}
+
+export const PRODUCTION_ENV = () => {
+  if (process.env.NODE_ENV != null) {
+    return process.env.NODE_ENV
   }
 }
